bcad565d408bcd045780191fa1dde63bcc90d32b
BinFench <bcfinch@edu.uwaterloo.ca>
2018-05-31 18:34:28 -0400

    Added undo and redo buttons

--- MaigePaint.py
+++ MaigePaint.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+from copy import deepcopy
 
 pygame.display.init()
 pygame.font.init()
@@ -131,12 +132,15 @@ def drawConfig():
     drawArt(width, height)
 
 def drawArt(x, y):
+    lastAction = "none"
+    indice = 0
+    maxIndice = 0
     width = x
     height = y
     pixelArray = [[None for i in range(width)] for j in range(height)]
     lastArray = []
-    lastArray.append(pixelArray)
-    colorArray = [(0,0,0),(128,128,128),(255,255,255),(128,0,0),(255,0,0),(0,128,0),(0,255,0),(0,0,128),(0,0,255),(255,128,0),(255,255,0),(255,0,128),(255,0,255),(0,255,128),(0,255,255),(128,255,0),(128,0,255)]
+    lastArray.append(deepcopy(pixelArray))
+    colorArray = [(255,255,255),(216,204,146),(175,137,92),(205,158,128),(160,95,91),(120,95,91),(0,0,0),(96,105,110),(145,146,150),(88,20,35),(157,19,42),(225,73,98),(250,125,139),(241,193,179),(233,198,158),(255,207,242),(218,110,160),(222,84,169),(199,16,140),(255,114,171),(155,138,242),(156,93,163),(124,86,171),(245,151,87),(238,106,57),(238,159,54),(219,206,13),(236,232,107),(209,226,36),(169,247,231),(181,242,165),(163,228,146),(130,183,49),(121,171,86),(84,151,74),(135,172,243),(85,135,198),(93,154,201),(124,155,219),(86,108,183),(4,40,124),(51,155,156),(155,212,221),(153,153,153),(223,172,153),(222,147,126),(223,212,71),(143,166,120),(147,198,193),(176,106,57),(147,77,26),(221,158,185),(249,133,110),(140,183,101),(152,198,211),(157,151,211),(182,182,182)]
     nextStage = False
     held = False
     currentColor = 0
@@ -166,6 +170,18 @@ def drawArt(x, y):
             if (20*page + i < len(colorArray)):
                 pygame.draw.rect(screen, colorArray[20*page + i], ((570 + 70 * (i % 4), 70 * int(i/4) + 10), (60, 60)))
 
+        pygame.draw.rect(screen, (255,255,255), ((570, 360), (40,40)))
+        pygame.draw.rect(screen, (255,255,255), ((520, 410), (40,40)))
+        pygame.draw.rect(screen, (255,255,255), ((190, 400), (180,80)))
+        pygame.draw.rect(screen, (255,255,255), ((105, 410), (60,60)))
+        pygame.draw.rect(screen, (255,255,255), ((400, 410), (60,60)))
+
+        screen.blit(text.render("+", 1, (0, 0, 0)), (576, 353))
+        screen.blit(text.render("+", 1, (0, 0, 0)), (526, 403))
+        screen.blit(text2.render("undo", 1, (0, 0, 0)), (110, 430))
+        screen.blit(text.render("%d x %d" % (width, height), 4, (0, 0, 0)), (200, 415))
+        screen.blit(text2.render("redo", 1, (0, 0, 0)), (405, 430))
+
         for i in range(x):
             for j in range(y):
                 if (pixelArray[i][j] != None):
@@ -174,26 +190,62 @@ def drawArt(x, y):
         if (pygame.mouse.get_pressed()[0]):
             mousex, mousey = pygame.mouse.get_pos()
             if ((mousex < 560) & (mousey < 400)):
+                lastAction = "draw"
                 held = True
                 (pixelArray[int(mousex/(560/x))])[int(mousey/(400/y))] = currentColor
+            elif ((mousex >= 105) & (mousex <= 165) & (mousey >= 410) & (mousey <= 470)):
+                if (held == False):
+                    held = True
+                    if (indice > 0):
+                        indice -= 1
+                        newPixelArray = deepcopy(lastArray[indice])
+                        newWidth = len(newPixelArray)
+                        newHeight = len(newPixelArray[0])
+                        pixelArray = deepcopy(newPixelArray)
+                lastAction = "undo"
+            elif ((mousex >= 400) & (mousex <= 460) & (mousey >= 410) & (mousey <= 470)):
+                if (held == False):
+                    held = True
+                    if (indice < maxIndice):
+                        indice += 1
+                        newPixelArray = deepcopy(lastArray[indice])
+                        newWidth = len(newPixelArray)
+                        newHeight = len(newPixelArray[0])
+                        pixelArray = deepcopy(newPixelArray)
+                lastAction = "redo"
             elif ((mousex > 560) & (mousey < 350)):
                 if (held == True):
+                    if (lastAction == "draw"):
+                        for i in range(maxIndice - indice):
+                            lastArray.pop()
+                        maxIndice = indice + 1
+                        indice = maxIndice
+                        lastArray.append(deepcopy(pixelArray))
                     held = False
-                    lastArray.append(pixelArray)
                 for i in range(20):
                     if (((mousex >= 570 + 70 * (i % 4)) and (mousex <= (570 + 70 * (i % 4) + 60)) and (mousey >= 10 + 70 * int(i/4)) and (mousey <= 70 + 70 * int(i/4)))):
                         if (20*page + i < len(colorArray)):
                             currentColor = 20*page + i
+                lastAction = "none"
             else:
                 if (held == True):
                     held = False
-                    lastArray.append(pixelArray)
-
+                    if (lastAction == "draw"):
+                        for i in range(maxIndice - indice):
+                            lastArray.pop()
+                        maxIndice = indice + 1
+                        indice += 1
+                        lastArray.append(deepcopy(pixelArray))
+                lastAction = "none"
         else:
             if (held == True):
                 held = False
-                lastArray.append(pixelArray)
-
+                if (lastAction == "draw"):
+                    for i in range(maxIndice - indice):
+                        lastArray.pop()
+                    maxIndice = indice + 1
+                    indice += 1
+                    lastArray.append(deepcopy(pixelArray))
         pygame.display.flip()
         #pygame.time.delay(17)
     
